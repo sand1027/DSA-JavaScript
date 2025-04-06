f0472366e2f576a6e1d3ae9166e5a7f14571834e
sand1027 <150948076+sand1027@users.noreply.github.com>
2025-04-06 13:16:28 +0530

    Time: 13 ms (59.93%) | Memory: 66.1 MB (66.97%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,11 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-nums.sort((a, b) => a-b)
-for(let i = 0; i<nums.length-1;i++){
-    if(nums[i] === nums[i+1]){
+const numSet = new Set()
+for(num of nums){
+    if(numSet.has(num)){
         return true
     }
+    numSet.add(num)
 }
 return false
 };
